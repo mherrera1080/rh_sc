ae9c039ab263d2dd7b4149584f8c38cefeb4ba9d
Melvin Herrera <m.herreramu@ccicsa.com.mx>
2025-12-10 12:34:32 -0600

    cambio guardar token 3

--- Libraries/Core/Conexion.php
+++ Libraries/Core/Conexion.php
@@ -1,23 +1,26 @@
 <?php
 class Conexion
 {
-	private $conect;
+    private $conect;
 
-	public function __construct()
-	{
-		$connectionString = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=" . DB_CHARSET;
-		try {
-			$this->conect = new PDO($connectionString, DB_USER, DB_PASSWORD);
-			$this->conect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			//echo "conexión exitosa";
-		} catch (PDOException $e) {
-			$this->conect = 'Error de conexión';
-			echo "ERROR: " . $e->getMessage();
-		}
-	}
+    public function __construct()
+    {
+        $connectionString = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=" . DB_CHARSET;
+        try {
+            $this->conect = new PDO($connectionString, DB_USER, DB_PASSWORD);
+            $this->conect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-	public function conect()
-	{
-		return $this->conect;
-	}
+            // Ajustar zona horaria de MySQL a Guatemala
+            $this->conect->exec("SET time_zone = '-06:00'");
+
+        } catch (PDOException $e) {
+            $this->conect = 'Error de conexión';
+            echo "ERROR: " . $e->getMessage();
+        }
+    }
+
+    public function conect()
+    {
+        return $this->conect;
+    }
 }
